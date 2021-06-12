b9bfba338c4d801afa26e2e45b2fd70edcfed3de
Sina Beyraghdar <59318782+SinaBYR@users.noreply.github.com>
2021-06-12 11:09:21 +0430

    Delete src directory

--- src/components/Navigation/NavigationLink/NavigationLink.js
+++
@@ -1,9 +0,0 @@
-import { NavLink } from "react-router-dom"
-
-const NavigationLink = props => {
-    return (
-        <li>
-            <NavLink to="/"></NavLink>
-        </li>
-    )
-}
--- src/components/UI/Container/Container.js
+++
@@ -1,9 +0,0 @@
-const Container = props => {
-    return (
-        <div style={{margin: props.margin, maxWidth: props.maxWidth, minWidth: props.minWidth, background: 'teal'}}>
-            {props.children}
-        </div>
-    )
-}
-
-export default Container;
