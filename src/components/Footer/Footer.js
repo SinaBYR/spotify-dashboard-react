a1af99c57efc8e542a3e686eaebc3e6a4f30e74c
Sina Beyraghdar <59318782+SinaBYR@users.noreply.github.com>
2021-06-12 11:16:23 +0430

    Add files via upload
    Finished Tasks:
    - LikedSongs
    - Albums
    - Artists
    - Profile Page
    - Performance Improved

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.css";
+
+const Footer = props => {
+    return (
+        <footer className={classes.Footer}>
+            <p>© 2021 Spotify Plus</p>
+        </footer>
+    )
+}
+
+export default Footer;
