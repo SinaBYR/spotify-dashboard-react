7e7dfbcb56f3e170bd347b4908e6a60983e03d9d
Sina Beyraghdar <59318782+SinaBYR@users.noreply.github.com>
2021-06-13 14:04:34 +0430

    Delete src directory

--- src/components/UI/Skeletons/ProfilePicSkeleton/ProfilePicSkeleton.js
+++
@@ -1,9 +0,0 @@
-import classes from './ProfilePicSkeleton.module.css';
-
-const ProfilePicSkeleton = () => {
-    return (
-        <div className={classes.ProfilePicSkeleton}></div>
-    )
-}
-
-export default ProfilePicSkeleton;
--- src/components/UI/Spinner/Spinner.js
+++
@@ -1,9 +0,0 @@
-import classes from "./Spinner.module.css";
-
-const Spinner = () => {
-    return (
-        <div className={classes.Spinner}>Loading...</div>
-    )
-}
-
-export default Spinner;
